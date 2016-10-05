69b215ff5141538529928bcbee6e319adc4c8d3d
nathanfaucett <nathanfaucett@gmail.com>
2016-10-05 15:57:11 -0500

    updates

--- bench/push.js
+++ bench/push.js
@@ -1,6 +1,6 @@
-var Benchmark = require("@nathanfaucett/benchmark"),
-    mori = require("@nathanfaucett/mori"),
-    Immutable = require("@nathanfaucett/immutable"),
+var Benchmark = require("benchmark"),
+    mori = require("mori"),
+    Immutable = require("immutable"),
     Vector = require("..");
 
 
